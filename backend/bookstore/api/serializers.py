e7987f42395d8392fbad795a32968b58537d5ce8
Chouaib-Djerdi <djerdichouaib@gmail.com>
2023-04-01 18:15:50 +0100

    add a comment from rest page and see it on the book detail page

--- backend/bookstore/api/serializers.py
+++ backend/bookstore/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from mainapp.models import Publisher,Author,Book
+from django.contrib.auth.models import User
 from .models import Comment
 
 class BookSerializer(serializers.ModelSerializer):
@@ -7,7 +8,14 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Comment
         fields = '__all__'
